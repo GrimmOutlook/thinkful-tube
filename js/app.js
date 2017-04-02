b3032377eb7292a0adc3a562079b32a75c180ab9
GrimmOutlook <grimmoutlook87@gmail.com>
2017-04-02 15:23:48 -0400

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+var endpointURL = "https://www.googleapis.com/youtube/v3/search";
+
+// Fxn to GET data from API
+
+
+
+
+
+// Callback Fxn that displays the data from API upon successful retrieval
+
+
+
+
+// Event Listener Fxn that, upon event trigger, calls the GET data fxn, passing in as arguments the user's input and the callback fxn.
+
+
+
+// Call the Event Listener Fxn???
